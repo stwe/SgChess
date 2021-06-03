b53132951483f4dcab31a9a3380722f90b0688d8
stwe <stwe@nomail.tt>
2021-06-03 15:32:45 +0200

    add tests

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -23,10 +23,16 @@ class BoardTest {
         var boardStart = new Board();
         assertTrue(boardStart.isQueenSideCastlingAllowed(Board.Color.WHITE));
         assertTrue(boardStart.isQueenSideCastlingAllowed(Board.Color.BLACK));
+        assertEquals(Bitboard.BOTH_CASTLE_BOTH_SIDES, boardStart.getCastlingRights());
 
         var board = new Board("rnbq1bnr/pppk1ppp/8/3pp3/4PP2/3P4/PPP3PP/RNBQKBNR w KQ - 0 1");
         assertTrue(board.isQueenSideCastlingAllowed(Board.Color.WHITE));
         assertFalse(board.isQueenSideCastlingAllowed(Board.Color.BLACK));
+        assertEquals(
+                Bitboard.WHITE_KING_CASTLE_KING_SIDE |
+                        Bitboard.WHITE_KING_CASTLE_QUEEN_SIDE,
+                board.getCastlingRights()
+        );
     }
 
     @Test
@@ -34,92 +40,113 @@ class BoardTest {
         var boardStart = new Board();
         assertTrue(boardStart.isKingSideCastlingAllowed(Board.Color.WHITE));
         assertTrue(boardStart.isKingSideCastlingAllowed(Board.Color.BLACK));
+        assertEquals(Bitboard.BOTH_CASTLE_BOTH_SIDES, boardStart.getCastlingRights());
 
         var board = new Board("rnbqkbnr/pp4pp/3ppp2/2p5/5PP1/3BP2N/PPPP3P/RNBQ1K1R w Qkq - 0 1");
         assertFalse(board.isKingSideCastlingAllowed(Board.Color.WHITE));
         assertTrue(board.isKingSideCastlingAllowed(Board.Color.BLACK));
+        assertEquals(
+                Bitboard.WHITE_KING_CASTLE_QUEEN_SIDE |
+                        Bitboard.BLACK_KING_CASTLE_KING_SIDE |
+                        Bitboard.BLACK_KING_CASTLE_QUEEN_SIDE,
+                board.getCastlingRights()
+        );
     }
 
     @Test
-    void makeCastlingMove() {
+    void makeCastlingMoveWhite() {
         var board = new Board("r3k2r/p3p2p/8/8/8/8/P3P2P/R3K2R w KQkq - 0 1");
 
-        var mg0 = new MoveGenerator(board);
-        mg0.generatePseudoLegalMoves();
-        var wmoves = mg0.getPseudoLegalMoves();
+        var mg = new MoveGenerator(board);
+        mg.generatePseudoLegalMoves();
+        var moves = mg.getPseudoLegalMoves();
 
         // start
         assertEquals(Bitboard.BOTH_CASTLE_BOTH_SIDES, board.getCastlingRights());
 
-        // white
-
         // white king castle king side
-        board.makeMove(wmoves.get(15));
+        board.makeMove(moves.get(15));
         var rest = Bitboard.BLACK_KING_CASTLE_KING_SIDE | Bitboard.BLACK_KING_CASTLE_QUEEN_SIDE;
         assertEquals(rest, board.getCastlingRights());
 
         // undo castling
-        board.undoMove(wmoves.get(15));
+        board.undoMove(moves.get(15));
         assertEquals(Bitboard.BOTH_CASTLE_BOTH_SIDES, board.getCastlingRights());
 
         // white king castle queen side
-        board.makeMove(wmoves.get(16));
+        board.makeMove(moves.get(16));
         rest = Bitboard.BLACK_KING_CASTLE_KING_SIDE | Bitboard.BLACK_KING_CASTLE_QUEEN_SIDE;
         assertEquals(rest, board.getCastlingRights());
 
         // undo castling
-        board.undoMove(wmoves.get(16));
-        assertEquals(Bitboard.BOTH_CASTLE_BOTH_SIDES, board.getCastlingRights());
-
-        // black
-
-        board.setColorToMove(Board.Color.BLACK);
-
-        var mg1 = new MoveGenerator(board);
-        mg1.generatePseudoLegalMoves();
-        var bmoves = mg1.getPseudoLegalMoves();
-
-        // black king castle king side
-        board.makeMove(bmoves.get(15));
-        rest = Bitboard.WHITE_KING_CASTLE_KING_SIDE | Bitboard.WHITE_KING_CASTLE_QUEEN_SIDE;
-        assertEquals(rest, board.getCastlingRights());
-
-        // undo castling
-        board.undoMove(bmoves.get(15));
-        assertEquals(Bitboard.BOTH_CASTLE_BOTH_SIDES, board.getCastlingRights());
-
-        // black queen castle queen side
-        board.makeMove(bmoves.get(16));
-        rest = Bitboard.WHITE_KING_CASTLE_KING_SIDE | Bitboard.WHITE_KING_CASTLE_QUEEN_SIDE;
-        assertEquals(rest, board.getCastlingRights());
-
-        // undo castling
-        board.undoMove(bmoves.get(16));
+        board.undoMove(moves.get(16));
         assertEquals(Bitboard.BOTH_CASTLE_BOTH_SIDES, board.getCastlingRights());
     }
 
     @Test
-    void makeMove() {
-        // todo
+    void makeCastlingMoveBlack() {
+        var board = new Board("r3k2r/p3p2p/8/8/8/8/P3P2P/R3K2R b KQkq - 0 1");
 
+        var mg = new MoveGenerator(board);
+        mg.generatePseudoLegalMoves();
+        var moves = mg.getPseudoLegalMoves();
+
+        // start
+        assertEquals(Bitboard.BOTH_CASTLE_BOTH_SIDES, board.getCastlingRights());
+
+        // black king castle king side
+        board.makeMove(moves.get(15));
+        var rest = Bitboard.WHITE_KING_CASTLE_KING_SIDE | Bitboard.WHITE_KING_CASTLE_QUEEN_SIDE;
+        assertEquals(rest, board.getCastlingRights());
+
+        // undo castling
+        board.undoMove(moves.get(15));
+        assertEquals(Bitboard.BOTH_CASTLE_BOTH_SIDES, board.getCastlingRights());
+
+        // black queen castle queen side
+        board.makeMove(moves.get(16));
+        rest = Bitboard.WHITE_KING_CASTLE_KING_SIDE | Bitboard.WHITE_KING_CASTLE_QUEEN_SIDE;
+        assertEquals(rest, board.getCastlingRights());
+
+        // undo castling
+        board.undoMove(moves.get(16));
+        assertEquals(Bitboard.BOTH_CASTLE_BOTH_SIDES, board.getCastlingRights());
+    }
+
+    @Test
+    void makePromotionCaptureMoveWhite() {
+        // f7e8=queen
         var board = new Board("k3n3/5P2/8/8/8/8/8/K7 w - - 0 1");
 
-        var mg0 = new MoveGenerator(board);
-        mg0.generatePseudoLegalMoves();
-        var moves = mg0.getPseudoLegalMoves();
-        System.out.println(board);
+        var mg = new MoveGenerator(board);
+        mg.generatePseudoLegalMoves();
+        var moves = mg.getPseudoLegalMoves();
 
         board.makeMove(moves.get(7));
-        System.out.println(board);
-
-        Bitboard.printBitboard(board.getBlackKnights());
-        Bitboard.printBitboard(board.getWhiteQueens());
+        assertEquals(0, board.getBlackKnights());
+        assertEquals(Piece.WHITE_QUEEN, board.getPieceFrom(Bitboard.BitIndex.E8_IDX));
 
         board.undoMove(moves.get(7));
-        System.out.println(board);
+        assertEquals(0, board.getWhiteQueens());
+        assertEquals(Piece.BLACK_KNIGHT, board.getPieceFrom(Bitboard.BitIndex.E8_IDX));
+    }
 
-        Bitboard.printBitboard(board.getBlackKnights());
-        Bitboard.printBitboard(board.getWhiteQueens());
+    @Test
+    void makePromotionCaptureMoveBlack() {
+        // d2c1=queen
+        var board = new Board("k7/8/8/8/8/8/3p4/2N4K b - - 0 1");
+
+        var mg = new MoveGenerator(board);
+        mg.generatePseudoLegalMoves();
+        var moves = mg.getPseudoLegalMoves();
+
+        board.makeMove(moves.get(7));
+        assertEquals(0, board.getWhiteKnights());
+        assertEquals(Piece.BLACK_QUEEN, board.getPieceFrom(Bitboard.BitIndex.C1_IDX));
+
+        board.undoMove(moves.get(7));
+        assertEquals(0, board.getBlackQueens());
+        assertEquals(Piece.WHITE_KNIGHT, board.getPieceFrom(Bitboard.BitIndex.C1_IDX));
     }
 
     @Test
